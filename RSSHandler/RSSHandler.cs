2e230f8b7ed39d8e41a85dbc31c215e9c8dfa299
ev <ev@evang.dk>
2014-11-12 10:02:07 +0100

    adds project to git

---
+++ RSSHandler/RSSHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RSSHandler
+{
+    public class RssHandler
+    {
+        /// <summary>
+        /// Load RSS feed into a xdocument
+        /// </summary>
+        /// <param name="rssFeed">a valid url to a rss feed</param>
+        /// <returns></returns>
+        public static XDocument LoadRSS(String rssFeed)
+        {
+            return XDocument.Load(rssFeed);
+        }
+
+        /// <summary>
+        /// retrieves all titles from a valid rss feed
+        /// </summary>
+        /// <param name="RssFeed">a rssfeed in xdocument</param>
+        /// <returns></returns>
+        public static List<String> RetrieveTitles(XDocument RssFeed)
+        {
+            return new List<string>();
+        }
+
+        public static String LatestArticle()
+        {
+            return "";
+        }
+
+
+    }
+}
---
+++ UnitTestProject1/TestRSSfeed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSSHandler;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestRSSfeed
+    {
+        [TestMethod]
+        public void TestLoadXML()
+        {
+            // test if the returning xdocument has a channel tag?
+
+            bool hasChannelTag = false;
+
+            string fakeRSS =
+                @"c:\users\ebva.rhs\documents\visual studio 2013\Projects\RSSHandler\UnitTestProject1\fakeRSSfeed.xml";
+            XDocument xdoc = RssHandler.LoadRSS(fakeRSS);
+
+            IEnumerable<XElement> items = xdoc.Descendants("item");
+
+            Assert.IsTrue(items.FirstOrDefault() != null);
+
+        }
+    }
+}
